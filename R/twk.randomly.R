f999afb2c241e06ea46f0946c5300e3d5dc80d1a
illoRocks <oliver_haag@gmx.net>
2018-11-11 04:07:54 +0100

    add randomly

---
+++ R/twk.randomly.R
@@ -0,0 +1,37 @@
+
+#' @importFrom purrr map
+NULL
+
+
+#' randomly
+#'
+#' Folding strategie
+#'
+#' @param data Training data
+#' @param method Name of folding strategie.
+#'   * cv Cross-Validation. k can be choosen (Default: 5)
+#' @param ... further arguments depends on the method
+#'
+#' @example
+#' set.seed(123)
+#'
+#' folds(iris, 'cv', k=5)
+#'
+#' @export
+randomly <- function(data, method="cv", ...) {
+  args <- list(...)
+
+  if (method=="cv") {
+
+    if (is.null(args$k))
+      args$k <- 5
+
+    n <- nrow(data)
+    in_test <- split(seq_len(n), sample(rep(1:args$k, length.out = n)))
+
+    return(map(in_test, ~(1:n)[-.x]))
+
+  }
+}
+
+
