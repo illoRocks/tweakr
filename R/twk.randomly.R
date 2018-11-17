af6b0a6acd5cefba5fad8675b7cf76889bdae6e3
illoRocks <oliver_haag@gmx.net>
2018-11-17 16:57:44 +0100

    change arguments

--- R/twk.randomly.R
+++ R/twk.randomly.R
@@ -8,10 +8,11 @@ NULL
 #' Folding strategie
 #'
 #' @param data Training data
-#' @param method Name of folding strategie.
+#' @param sample_method Name of folding strategie.
 #' \itemize{
 #'   \item \strong{cv} Cross-Validation. k can be choosen (Default: 5)
 #' }
+#' @param k Number of folds.
 #' @param ... further arguments depends on the method.
 #'
 #' @return List of indices in train data.
@@ -21,17 +22,17 @@ NULL
 #' folds <- randomly(iris, 'cv', k=5)
 #'
 #' @export
-randomly <- function(data, method="cv", ...) {
-  args <- list(...)
+randomly <- function(data, sample_method="cv", k=5, ...) {
 
-  if (method=="cv") {
+  if (sample_method=="cv") {
 
-    k <- get_value(args$k, 5)
     n <- nrow(data)
     in_test <- split(seq_len(n), sample(rep(1:k, length.out = n)))
     return(map(in_test, ~(1:n)[-.x]))
 
   }
+
+  stop("no valid sample method")
 }
 
 
